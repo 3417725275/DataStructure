121bc3667f30adadddec0633ba9be7ade3da7173
3417725275 <3417725275@qq.com>
2021-03-01 18:17:22 +0800

    修改2

--- DataStructure/BinaryTreeNode.h
+++ DataStructure/BinaryTreeNode.h
@@ -3,10 +3,13 @@
 
 #ifndef _BinaryTreeNode_H
 #define _BinaryTreeNode_H
+#include <iostream>
+
 
 template <typename T>
 class BinaryTreeNode
 {
+public:
 	//构造
 	BinaryTreeNode() :leftchild(nullptr), rightchild(nullptr) {}
 	BinaryTreeNode(const T& dt, const BinaryTreeNode<T>* &lc, const BinaryTreeNode<T>* &rc) :data(dt), leftchild(lc), rightchild(rc) {}
@@ -16,28 +19,28 @@ class BinaryTreeNode
 	BinaryTreeNode(const BinaryTreeNode<T> & x);
 
 	//获取成员值
-	T GetData() { return this!=nullptr? data:-1; }
-	BinaryTreeNode<T>* Getleftchild() const{ return this!=nullptr?leftchild:nullptr; }
-	BinaryTreeNode<T>* Getrightchild() const{ return this!=nullptr?rightchild:nullptr }
+	T GetData() { return this != nullptr ? data : -1; }
+	BinaryTreeNode<T>* Getleftchild() const { return this != nullptr ? leftchild : nullptr; }
+	BinaryTreeNode<T>* Getrightchild() const { return this != nullptr ? rightchild : nullptr; }
 
 	//设置成员值
 	void SetData(const T& dt)
 	{
-		if (this!=nullptr)
+		if (this != nullptr)
 		{
 			data = dt;
 		}
 	}
 	void Setleftchild(const BinaryTreeNode<T>* &lc)
 	{
-		if (this!=nullptr)
+		if (this != nullptr)
 		{
 			leftchild = lc;
 		}
 	}
 	void Setrightchild(const BinaryTreeNode<T>* &rc)
 	{
-		if (this!=nullptr)
+		if (this != nullptr)
 		{
 			rightchild = rc;
 		}
@@ -45,7 +48,7 @@ class BinaryTreeNode
 
 	//拷贝节点
 	BinaryTreeNode<T>* Copy(const BinaryTreeNode<T>* x);
-
+	
 	//销毁以本节点为根的子树
 	void Destory()
 	{
@@ -57,6 +60,18 @@ class BinaryTreeNode
 		}
 	}
 
+	//高度和节点数量
+	int height();
+	int size();
+
+	//相等性判断
+	friend bool equal(const BinaryTreeNode<T>* a, const BinaryTreeNode<T>* b);
+
+
+	//前中后遍历
+	void preOrder();//前序遍历
+	void InOrder();//中序遍历
+	void PostOrder();//后序遍历
 
 private:
 	T data;
@@ -70,7 +85,7 @@ BinaryTreeNode<T>::BinaryTreeNode(const BinaryTreeNode<T>& x)
 {
 	if (x != *this)
 	{
-		this->data= x.data;
+		this->data = x.data;
 		BinaryTreeNode<T>* temp = leftchild;
 		this->leftchild = x.leftchild;
 		delete temp;
@@ -80,6 +95,85 @@ BinaryTreeNode<T>::BinaryTreeNode(const BinaryTreeNode<T>& x)
 		temp = nullptr;
 	}
 
+}
+
+template<typename T>
+BinaryTreeNode<T>* BinaryTreeNode<T>::Copy(const BinaryTreeNode<T>* x)
+{
+	if (x == nullptr)
+		return nullptr;
+
+	BinaryTreeNode<T>* temp = new BinaryTreeNode<T>(x->data);
+	temp->leftchild = Copy(x->leftchild);
+	temp->rightchild = Copy(x->rightchild);
+	return temp;
+}
+
+template<typename T>
+int BinaryTreeNode<T>::height()
+{
+	if (this != nullptr)
+	{
+		int lftheight = this->leftchild->height();
+		int rhtheight = this->rightchild->height();
+		return (1 + lftheight > rhtheight ? lftheight : rightchild);
+	}
+	return 0;
+}
+
+template<typename T>
+int BinaryTreeNode<T>::size()
+{
+	if (this != nullptr)
+	{
+		return 1 + this->leftchild->size() + this->rightchild->size();
+	}
+	return 0;
+}
+
+template<typename T>
+void BinaryTreeNode<T>::preOrder()
+{
+	if (this != nullptr)
+	{
+		std::cout << "--->" << this.data << std::endl;
+		this->leftchild->preOrder();
+		this->rightchild->preOrder();
+	}
+}
+
+template<typename T>
+void BinaryTreeNode<T>::InOrder()
+{
+	if (this != nullptr)
+	{
+		this->leftchild->preOrder();
+		std::cout << "--->" << this.data << std::endl;
+		this->rightchild->preOrder();
+	}
+}
+
+template<typename T>
+void BinaryTreeNode<T>::PostOrder()
+{
+	if (this != nullptr)
+	{
+		this->leftchild->preOrder();
+		this->rightchild->preOrder();
+		std::cout << "--->" << this.data << std::endl;
+	}
+}
+
+template<typename T>
+bool equal(const BinaryTreeNode<T>* a, const BinaryTreeNode<T>* b)
+{
+	if (a == nullptr && b == nullptr)
+		return true;
+	if (a == nullptr || b == nullptr)
+		return false;
+	if (a->data == b->data && equal(a->leftchild, b->leftchild) && equal(a->rightchild, b->rightchild))
+		return true;
+	return false;
 }
 
 
--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -2,7 +2,6 @@
 //
 
 #include "stdafx.h"
-#include <iostream>
 #include "BinaryTreeNode.h"
 
 int main()
