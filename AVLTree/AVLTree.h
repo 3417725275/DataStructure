a3fa5fdc200e2519edda840ef8b6086fcb6260e9
3417725275 <3417725275@qq.com>
2021-03-04 16:28:09 +0800

    AVL树

--- AVLTree/AVLTree.h
+++ AVLTree/AVLTree.h
@@ -11,16 +11,98 @@ class AVLTree :public BinaryTree
 	void insert(const T& elem);
 	void erase(const T& elem);
 
-	BinaryTreeNode<T>* single_rotate_with_left(BinaryTreeNode<T>* pTn);
-	BinaryTreeNode<T>* single_rotate_with_right(BinaryTreeNode<T>* pTn);
-	BinaryTreeNode<T>* double_rotate_with_left(BinaryTreeNode<T>* pTn);
-	BinaryTreeNode<T>* double_rotate_with_right(BinaryTreeNode<T>* pTn);
+	//LL单旋，x是最小不平衡二叉树的根节点
+	BinaryTreeNode<T>* LL_rotate(BinaryTreeNode<T>* x);
+	//RR单旋，x是最小不平衡二叉树的根节点
+	BinaryTreeNode<T>* RR_rotate(BinaryTreeNode<T>* x);
+	//LR双旋，x是最小不平衡二叉树的根节点
+	BinaryTreeNode<T>* LR_rotate(BinaryTreeNode<T>* x);
+	//RL双旋，x是最小不平衡二叉树的根节点
+	BinaryTreeNode<T>* RL_rotate(BinaryTreeNode<T>* x);
 
 private:
-	BinaryTreeNode<T>* insert(const T& elem, BinaryTreeNode<T>* pTn);
-	BinaryTreeNode<T>* erase(const T& elem, BinaryTreeNode<T>* pTn);
-
+	//在以x为根的平衡二叉树中插入elem
+	BinaryTreeNode<T>* insert(const T& elem, BinaryTreeNode<T>* x);
+	//在以x为根的平衡二叉树中删除elem
+	BinaryTreeNode<T>* erase(const T& elem, BinaryTreeNode<T>* x);
 
 };
+
+
+
+template<typename T>
+BinaryTreeNode<T>* AVLTree<T>::LL_rotate(BinaryTreeNode<T>* x)
+{
+	BinaryTreeNode<T>* newroot = x->Getleftchild();
+	x->Setleftchild(newroot->Getrightchild());
+	newroot->Setrightchild(x);
+	return newroot;
+}
+
+template<typename T>
+BinaryTreeNode<T>* AVLTree<T>::RR_rotate(BinaryTreeNode<T>* x)
+{
+	BinaryTreeNode<T>* newroot = x->Getrightchild();
+	x->Setrightchild(newroot->Getleftchild());
+	newroot->Setleftchild(x);
+	return newroot;
+}
+
+template<typename T>
+BinaryTreeNode<T>* AVLTree<T>::LR_rotate(BinaryTreeNode<T>* x)
+{
+	x->Setleftchild(RR_rotate(x->Getleftchild()));
+	return LL_rotate(x);
+}
+
+template<typename T>
+BinaryTreeNode<T>* AVLTree<T>::RL_rotate(BinaryTreeNode<T>* x)
+{
+	x->Setrightchild(LL_rotate(x->Getrightchild()));
+	return RR_rotate(x);
+}
+
+template<typename T>
+BinaryTreeNode<T>* AVLTree<T>::insert(const T & elem, BinaryTreeNode<T>* x)
+{
+	if (x == nullptr)
+	{
+		x = new BinaryTreeNode<T>(elem);
+		return x;
+	}
+	if (elem == x->GetData())
+		return x;
+	if (elem < x->GetData())
+	{
+		x->Setleftchild(insert(elem, x->Getleftchild());
+		//这里补充一个判断插入后是否是平衡二叉树，如果不是，要调整
+
+	}
+	else
+	{
+		x->Setrightchild(insert(elem, x->Getrightchild()));
+		//这里补充一个判断插入后是否是平衡二叉树，如果不是，要调整
+	}
+
+	
+			if (2 == depth(x->lchild) - depth(x->rchild)) {
+				if (elem < x->lchild->element)
+					x = single_rotate_with_left(x);
+				else
+					x = double_rotate_with_left(x);
+			}
+		}//else if
+		else {	//elem > x->element
+			x->rchild = insert(elem, x->rchild);
+			if (2 == depth(x->rchild) - depth(x->lchild)) {
+				if (elem > x->rchild->element)
+					x = single_rotate_with_right(x);
+				else
+					x = double_rotate_with_right(x);
+			}
+		}//else
+	}//else
+	return x;
+}
 
 #endif
---
+++ AVLTree/BinaryTreeNode.h
@@ -0,0 +1,199 @@
+#pragma once
+
+#ifndef _BinaryTreeNode_H_
+#define _BinaryTreeNode_H_
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode
+{
+public:
+	//构造
+	BinaryTreeNode() :leftchild(nullptr), rightchild(nullptr) {}
+	BinaryTreeNode(const T& dt,  BinaryTreeNode<T>* lc,  BinaryTreeNode<T>* rc) :data(dt), leftchild(lc), rightchild(rc) {}
+	BinaryTreeNode(const T& dt) :BinaryTreeNode(dt, nullptr, nullptr) {}
+
+	//拷贝构造
+	BinaryTreeNode(const BinaryTreeNode<T> & x);
+
+	//获取成员值
+	T GetData() { return this != nullptr ? data : -1; }
+	BinaryTreeNode<T>* Getleftchild() const { return this != nullptr ? leftchild : nullptr; }
+	BinaryTreeNode<T>* Getrightchild() const { return this != nullptr ? rightchild : nullptr; }
+
+	//设置成员值
+	void SetData(const T& dt)
+	{
+		if (this != nullptr)
+		{
+			data = dt;
+		}
+	}
+	void Setleftchild(BinaryTreeNode<T>* lc)
+	{
+		if (this != nullptr)
+		{
+			leftchild = lc;
+		}
+	}
+	void Setrightchild(BinaryTreeNode<T>* rc)
+	{
+		if (this != nullptr)
+		{
+			rightchild = rc;
+		}
+	}
+
+	//拷贝节点
+	BinaryTreeNode<T>* Copy(const BinaryTreeNode<T>* x);
+
+	//销毁以本节点为根的子树
+	void Destory()
+	{
+		if (this != nullptr)
+		{
+			this->leftchild->Destory();
+			this->rightchild->Destory();
+			delete this;
+		}
+	}
+
+	//高度和节点数量
+	int height()const;
+	int size() const;//以本节点为根的子树的节点数
+
+	//相等性判断
+	friend bool equal(const BinaryTreeNode<T>* a, const BinaryTreeNode<T>* b);
+
+
+	//前中后遍历
+	void preOrder() const;//前序遍历
+	void InOrder() const;//中序遍历
+	void PostOrder() const;//后序遍历
+	void levelOrder() const;//层次遍历
+
+private:
+	T data;
+	BinaryTreeNode<T>* leftchild, *rightchild;
+};
+
+
+template<typename T>
+BinaryTreeNode<T>::BinaryTreeNode(const BinaryTreeNode<T>& x)
+{
+	if (x != *this)
+	{
+		this->data = x.data;
+		BinaryTreeNode<T>* temp = leftchild;
+		this->leftchild = x.leftchild;
+		delete temp;
+		temp = rightchild;
+		this->rightchild = x.rightchild;
+		delete temp;
+		temp = nullptr;
+	}
+
+}
+
+template<typename T>
+BinaryTreeNode<T>* BinaryTreeNode<T>::Copy(const BinaryTreeNode<T>* x)
+{
+	if (x == nullptr)
+		return nullptr;
+
+	BinaryTreeNode<T>* temp = new BinaryTreeNode<T>(x->data);
+	temp->leftchild = Copy(x->leftchild);
+	temp->rightchild = Copy(x->rightchild);
+	return temp;
+}
+
+template<typename T>
+int BinaryTreeNode<T>::height() const
+{
+	if (this != nullptr)
+	{
+		int lftheight = this->leftchild->height();
+		int rhtheight = this->rightchild->height();
+		return (1 + lftheight > rhtheight ? lftheight : rightchild);
+	}
+	return 0;
+}
+
+template<typename T>
+int BinaryTreeNode<T>::size() const
+{
+	if (this != nullptr)
+	{
+		return 1 + this->leftchild->size() + this->rightchild->size();
+	}
+	return 0;
+}
+
+template<typename T>
+void BinaryTreeNode<T>::preOrder() const
+{
+	if (this != nullptr)
+	{
+		std::cout << "--->" << this.data << std::endl;
+		this->leftchild->preOrder();
+		this->rightchild->preOrder();
+	}
+}
+
+template<typename T>
+void BinaryTreeNode<T>::InOrder() const
+{
+	if (this != nullptr)
+	{
+		this->leftchild->preOrder();
+		std::cout << "--->" << this.data << std::endl;
+		this->rightchild->preOrder();
+	}
+}
+
+template<typename T>
+void BinaryTreeNode<T>::PostOrder() const
+{
+	if (this != nullptr)
+	{
+		this->leftchild->preOrder();
+		this->rightchild->preOrder();
+		std::cout << "--->" << this.data << std::endl;
+	}
+}
+
+template<typename T>
+void BinaryTreeNode<T>::levelOrder() const
+{
+	if (this == nullptr)
+		return;
+	queue<BinaryTreeNode<T>*> nodeQueue;
+	nodeQueue.push(this);
+	BinaryTreeNode<T>* temp;
+	while (!nodeQueue.empty())
+	{
+		temp = nodeQueue.front();
+		std::cout << "--->" << temp.data << std::endl;
+		if (temp = temp->Getleftchild())
+			nodeQueue.push(temp);
+		if (temp = temp->Getrightchild())
+			nodeQueue.push(temp);
+		nodeQueue.pop();
+	}
+}
+
+template<typename T>
+bool equal(const BinaryTreeNode<T>* a, const BinaryTreeNode<T>* b)
+{
+	if (a == nullptr && b == nullptr)
+		return true;
+	if (a == nullptr || b == nullptr)
+		return false;
+	if (a->data == b->data && equal(a->leftchild, b->leftchild) && equal(a->rightchild, b->rightchild))
+		return true;
+	return false;
+}
+
+
+#endif
